a20b8d4e82261df60fea6ce8d5a62ed19e7a29ed
Wesley Medeiros Ramalho <wesley.medeiros6938@gmail.com>
2023-07-04 23:30:11 -0300

    add home structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import "../styles/pages/page.scss"
+import Camiseta1 from "../assets/1.png"
+import Camiseta2 from "../assets/2.png"
+import Camiseta3 from "../assets/3.png"
+
 export default function Home() {
   return (
-    <h1>Olá</h1>
+    <main>
+      <a href="">
+        <Image src={Camiseta1} width={520} height={480} alt={""} />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </a>
+
+      <a href="">
+        <Image src={Camiseta2} width={520} height={480} alt={""} />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </a>
+    </main>
   )
 }
